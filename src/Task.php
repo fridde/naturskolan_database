e9e15203004fe4cb7f58089075a3354718f293ef
fridde <f@hehl.se>
2016-10-29 18:12:43 +0200

    Further work

--- src/Task.php
+++ src/Task.php
@@ -4,8 +4,10 @@ namespace Fridde;
 class Task extends Naturskolan
 {
     public $type;
-    public $current_status;
-    public $changed_status;
+    public $tables;
+    public $result = false;
+    public $now;
+    public $admin_mail = [];
     private $task_to_function_map = [
         "calendar_rebuild" => "rebuildCalendar",
         "mailchimp_sync" => "syncMailchimp",
@@ -17,22 +19,18 @@ class Task extends Naturskolan
         "table_cleanup" => "cleanSQLDatabase"
     ];
 
-    function __construct ($type, $current_status = [])
+    function __construct ($type, $tables = [])
     {
         $this->type = $task_type;
-        $this->current_status = $current_status;
-    }
-
-    public function getChangedStatus()
-    {
-        return $this->changed_status;
+        $this->tables = $tables;
+        $this->now =  C::now();
     }
 
     public function execute()
     {
         $function_name = $this->task_to_function_map[$this->type];
         $this->$function_name;
-
+        return $this->result;
     }
 
     private function rebuildCalendar()
@@ -49,7 +47,7 @@ class Task extends Naturskolan
             }
             $cal = new Calendar($tables);
             $cal->save();
-            $this->changed_status = ["calendar.status" => "clean", "calendar.last_rebuild" => $now->toIso8601String()];
+            $this->result = ["calendar.status" => "clean", "calendar.last_rebuild" => $now->toIso8601String()];
         }
     }
 
@@ -65,16 +63,135 @@ class Task extends Naturskolan
     {
     }
 
+    private function addToAdminMail($type, $info = []){
+        $tables = $this->tables;
+        $text = "";
+
+        switch($type){
+            case "visit_not_confirmed":
+            $topic = U::filterFor($tables["topics"], ["id", $info["Topic"]]);
+            $group = U::filterFor($tables["groups"], ["id", $info["Group"]]);
+            $school = U::filterFor($tables["schools"], ["id", $group["School"]]);
+            $user = U::filterFor($tables["users"], ["id", $group["User"]]);
+
+            $text .= $info["Date"] .  ": ";
+            $text .= $topic["ShortName"] . " med ";
+            $text .= $group["Name"] . " från ";
+            $text .= $school["Name"] . " Lärare: ";
+            $text .= $user["FirstName"] . " " . $user["LastName"] . ", ";
+            $text .= $user["Mobil"] . ", " . $user["Mail"];
+            break;
+
+            case ""
+        }
+        $this->admin_mail[$type][] = $text;
+    }
+
     private function sendAdminSummaryMail()
     {
+        foreach($admin_mail as $type => $rows){
+            $pre_text = "";
+
+        }
+    }
+
+    private function compileAdminSummaryMail()
+    {
+        $settings = $SETTINGS["admin_summary"];
+
+        $tables = $this->tables;
+
+        // visit not confirmed
+        foreach($tables["visits"] as $visit){
+            $visit_date = new C($visit["Date"]);
+            $days_left = $visit_date->diffInDays($this->now);
+            $close_enough = $settings["no_confirmation_warning"];
+            $confirmed = $visit["Confirmed"] == 1;
+
+            if($days_left >= 0 && $days_left <= $settings["no_confirmation_warning"] && !$confirmed){
+                $this->addToAdminMail("visit_not_confirmed", $visit);
+            }
+        }
+
+        // food or number of students changed
+        $criteria[] = ["Table_name", "groups"];
+        $criteria[] = ["Column_name", "in", ["Food", "Students"]];
+        $field_history = U::filterFor($tables["field_history"], $criteria);
+        foreach($field_history as $hist_field){
+            $group = U::filterFor($tables["groups"], ["id", $hist_field["Row_id"]]);
+            $group_visits
+        }
+
+     /*
+      ###
+      any field_history where (Table == "groups" && (Column in(["Food", "Students"])
+        group = select_where(group[id] == field_history[row_id])
+        group_visits = select_where(visit[group] == group[id] && visit[date] >= now)
+        next_visit_date = min(group_visits[date])
+        column = field_history[Column]
+        if(field_history[value] != group[column] && diff(next_visit_date, now) < 2 weeks)
+          add to mail(group, column, value, next_visit_date)
+        delete field_history
+
+      ### bus not in sync
+
+      ### less than 60days to last booked visit
+      if(diff(max visit[date], now) < 60 days))
+        add to mail(diff)
+
+      ### wrong amount of groups
+      any school
+        any arskurs
+          count_is = count_where(group[arskurs] == arskurs)
+          count_should = school[arskurs]
+          if(count_is != count_should)
+            add to mail(count_is, count_should, school, arskurs)
+
+      ###group leader is not teacher
+      any group
+        group_leader = select_where(group[user] == user[id])
+        if(group_leader[type] == "admin")
+          add to mail(group, user)
+
+
+      ### user profile not complete
+      any user
+        reminder_messages = select_where(message[to] == user[id] && message[type] == "reminder")
+        latest_reminder_date = max(reminder_messages[date])
+        if (user[created] more than 1 week ago && (user[mobil] == "" || user[mail] == "") && diff(now, latest_reminder_date) > 4 days)
+           add to mail(user name)
+
+
+      send_mail(admin, admin_mail)
+      */
     }
 
     private function sendChangedGroupLeaderMail()
     {
+        /*
+      any field_history where (Table == "groups" && Column == "User")
+        group = select_where(group[user] == value)
+        if(group[user] != value)
+          send_mail(group[user], group)
+        */
+    }
+
+    private function changedVisitDateMail()
+    {
+    /*
+      any field_history where (Table == "visits" && Column == "Date")
+        visit = select_where(visit[Date] == value)
+        if(visit[Date] != value)
+          send_mail(...
+          */
     }
 
     private function sendProfileUpdateReminderMail()
     {
+    /*
+      any user
+        if
+        */
     }
 
     private function cleanSQLDatabase()
