cff8a14a3c77253257a545e00e54850916e68a77
fridde <f@hehl.se>
2018-10-20 23:51:08 +0200

    More work

--- debug_functions.php
+++ debug_functions.php
@@ -2,7 +2,7 @@
 
 define('BEFORE', microtime(true));
 function toConsole($txt){
-    echo '<script>console.log("' . $txt .'")</script>';
+    echo '<script>console.log(\'' . $txt .'\')</script>';
 }
 function showDebugTime(){
     $txt = round(microtime(true) - BEFORE, 2) . ' seconds';
