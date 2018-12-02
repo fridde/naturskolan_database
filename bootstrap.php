fe6f27e481b181ed57575c55c349cb4426f2f62a
fridde <f@hehl.se>
2018-12-02 12:51:40 +0100

    More work

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,7 @@ require __DIR__.'/vendor/autoload.php';
 use Doctrine\Common\Cache\FilesystemCache;
 use Fridde\Essentials;
 use Fridde\Naturskolan;
+use Fridde\Router;
 use Fridde\Settings;
 use Carbon\Carbon;
 
@@ -27,7 +28,7 @@ $services[] = ['Naturskolan', Naturskolan::class];
 $controller_namespace = '\\Fridde\\Controller';
 
 $base_url = rtrim(parse_url(APP_URL, PHP_URL_PATH), '/');
-$services[] = ['Router', \Fridde\Router::class, $base_url, Essentials::getRoutes(), $controller_namespace];
+$services[] = ['Router', Router::class, $base_url, Essentials::getRoutes(), $controller_namespace];
 $services[] = ['Logger', Essentials::getLogger()];
 $services[] = ['Cache', new FilesystemCache(__DIR__ . '/temp/cache')];
 $container = Essentials::registerSharedServices($services);
