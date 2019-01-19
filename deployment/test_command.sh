48c412bfc98c7cdb934e21fcba17530f72ac7dd2
fridde <f@hehl.se>
2019-01-19 17:48:22 +0100

    More work

--- deployment/test_command.sh
+++ deployment/test_command.sh
@@ -1,3 +1,2 @@
-#!/usr/bin/env bash
-
+cd ~/testing/naturskolan_database
 A=": && "$(echo "codecept run acceptance "{Index,SchoolPage,Admin,Cron,Table,Permission}"Cest --steps -f &&")" :" && eval echo $A
